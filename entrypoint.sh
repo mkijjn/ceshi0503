dc0c7d44ad9f825aad24b24f94b67751b9359776
mkijjn <131351649+mkijjn@users.noreply.github.com>
2023-06-13 18:46:32 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ WSPATH=${WSPATH:-'argo'}
 UUID=${UUID:-'a7c33da5-b0ba-4191-b2d8-9aad45b28747'}
 
 # 其他Paas保活
+PAAS4_URL=https://p02--test052706--bz7b69b6gjkz.code.run
 PAAS1_URL=https://back0503-n3jritmw.b4a.run
 PAAS2_URL=https://rendrr0503.onrender.com 
 PAAS3_URL=https://app-0ec68291-3f3b-4882-a6cf-db75aa008910.cleverapps.io
